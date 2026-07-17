b6ad383aef9c5218f351d3e03f347105d5d9dfe7
MuriloMedeiros7 <murilo91321@gmail.com>
2026-07-17 15:57:51 -0400

    murilo fez a funcao de uma calculadora no funcao_calculadora.py

---
+++ funcao_calculadora.py
@@ -0,0 +1,16 @@
+def calculadora(a, b, operacao):
+    if operacao == 1:
+        return a+b
+    
+    elif operacao == 2:
+        return a-b
+    
+    elif operacao == 3:
+        return a*b
+    
+    elif operacao == 4:
+
+        return a/b
+    
+    else:
+        print("Valor Inválido")
