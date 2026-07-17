b08a5b7859d8c6c43f14f6449c4c2384bc3b9b41
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-07-17 17:01:24 -0400

    Chamando a minha função na historia.py

--- par_ou_impar.py
+++ par_ou_impar.py
@@ -3,3 +3,14 @@ def par(n):
         return True
     else:
         return False
+
+def rodar():
+    while True:
+        num = int (input("\nInsira um número: "))
+        if par(num):
+            print ("\nÉ par")
+        else:
+            print ("\nÉ impar")
+        
+if __name__ == "__main__":
+    rodar()
